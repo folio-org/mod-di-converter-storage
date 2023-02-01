ce4b7e02e919341f8ad2b64eece1f56bb3d8699e
aliaksandr_fedasiuk <aliaksandr_fedasiuk@epam.com>
2023-02-01 15:15:36 +0500

    MODDICONV-291: fix FATAL: password authentication failed for user "mod_di_converter_storage"

--- mod-di-converter-storage-server/src/main/java/org/folio/rest/impl/util/ExceptionHelper.java
+++ mod-di-converter-storage-server/src/main/java/org/folio/rest/impl/util/ExceptionHelper.java
@@ -42,7 +42,7 @@ public final class ExceptionHelper {
         .entity(throwable.getMessage())
         .build();
     }
-    LOGGER.warn("{}", throwable.getMessage(), throwable);
+    LOGGER.error("{}", throwable.getMessage(), throwable);
     Promise<Response> validationFuture = Promise.promise();
     ValidationHelper.handleError(throwable, validationFuture);
     if (validationFuture.future().isComplete()) {
