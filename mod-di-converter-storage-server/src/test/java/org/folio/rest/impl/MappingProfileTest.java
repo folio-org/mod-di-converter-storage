72068d6b4c0cbc1be2e975f6f2e071557be68883
Roman Chernetskyi <Roman_Chernetskyi@epam.com>
2023-05-09 12:03:45 +0300

    [MODDICONV-303] Return 404 on get profile by id if profile was marked as deleted

--- mod-di-converter-storage-server/src/test/java/org/folio/rest/impl/MappingProfileTest.java
+++ mod-di-converter-storage-server/src/test/java/org/folio/rest/impl/MappingProfileTest.java
@@ -554,8 +554,16 @@ public class MappingProfileTest extends AbstractRestVerticleTest {
       .when()
       .get(MAPPING_PROFILES_PATH + "/" + profile.getProfile().getId())
       .then()
+      .statusCode(HttpStatus.SC_NOT_FOUND);
+
+    RestAssured.given()
+      .spec(spec)
+      .when()
+      .get(MAPPING_PROFILES_PATH + "?showDeleted=true")
+      .then()
       .statusCode(HttpStatus.SC_OK)
-      .body("deleted", is(true));
+      .body("totalRecords", is(1))
+      .body("mappingProfiles.get(0).deleted", is(true));
   }
 
   @Test
--- mod-di-converter-storage-server/src/test/java/org/folio/rest/impl/MatchProfileTest.java
+++ mod-di-converter-storage-server/src/test/java/org/folio/rest/impl/MatchProfileTest.java
@@ -482,8 +482,16 @@ public class MatchProfileTest extends AbstractRestVerticleTest {
       .when()
       .get(MATCH_PROFILES_PATH + "/" + profile.getProfile().getId())
       .then()
+      .statusCode(HttpStatus.SC_NOT_FOUND);
+
+    RestAssured.given()
+      .spec(spec)
+      .when()
+      .get(MATCH_PROFILES_PATH + "?showDeleted=true")
+      .then()
       .statusCode(HttpStatus.SC_OK)
-      .body("deleted", is(true));
+      .body("totalRecords", is(1))
+      .body("matchProfiles.get(0).deleted", is(true));
   }
 
   @Test
