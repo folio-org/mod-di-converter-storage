1b9cd89c2677eed983457e2073e1c0d34dc72855
Volodymyr_Rohach <Volodymyr_Rohach@epam.com>
2023-06-01 15:54:09 +0300

    MODDICONV-300: Minor improvements.

--- mod-di-converter-storage-server/src/main/java/org/folio/services/AbstractProfileService.java
+++ mod-di-converter-storage-server/src/main/java/org/folio/services/AbstractProfileService.java
@@ -23,11 +23,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Optional;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -384,8 +383,8 @@ public abstract class AbstractProfileService<T, S, D> implements ProfileService<
     return promise.future();
   }
 
-  private static <T> Predicate<T> distinctByKeys(final Function<? super T, ?>... keyExtractors) {
-    final Map<List<?>, Boolean> seen = new ConcurrentHashMap<>();
+  private <B> Predicate<B> distinctByKeys(final Function<? super B, ?>... keyExtractors) {
+    final Map<List<?>, Boolean> seen = new HashMap<>();
     return e ->
     {
       final List<?> keys = Arrays.stream(keyExtractors)
