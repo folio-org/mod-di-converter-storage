2a5c5a900d5ba046e08c7ccc8a992d4fbb5b4659
Volodymyr_Rohach <Volodymyr_Rohach@epam.com>
2023-08-22 15:27:55 +0300

    IF NOT EXISTS added.

--- mod-di-converter-storage-server/src/main/resources/templates/db_scripts/associations-migration/profile_wrappers_table.sql
+++ mod-di-converter-storage-server/src/main/resources/templates/db_scripts/associations-migration/profile_wrappers_table.sql
@@ -3,11 +3,11 @@ ON ${myuniversity}_${mymodule}.profile_wrappers CASCADE;
 
 ALTER TABLE ${myuniversity}_${mymodule}.profile_wrappers
   DROP COLUMN IF EXISTS jsonb,
-  ADD profile_type text NULL,
-  ADD action_profile_id uuid NULL,
-  ADD match_profile_id uuid NULL,
-  ADD mapping_profile_id uuid NULL,
-  ADD job_profile_id uuid NULL,
+  ADD IF NOT EXISTS profile_type text NULL,
+  ADD IF NOT EXISTS action_profile_id uuid NULL,
+  ADD IF NOT EXISTS match_profile_id uuid NULL,
+  ADD IF NOT EXISTS mapping_profile_id uuid NULL,
+  ADD IF NOT EXISTS job_profile_id uuid NULL,
   ADD CONSTRAINT profile_wrappers_action_profile_fk FOREIGN KEY (action_profile_id) REFERENCES ${myuniversity}_${mymodule}.action_profiles(id),
   ADD CONSTRAINT profile_wrappers_match_profile_fk FOREIGN KEY (match_profile_id) REFERENCES ${myuniversity}_${mymodule}.match_profiles(id),
   ADD CONSTRAINT profile_wrappers_mapping_profile_fk FOREIGN KEY (mapping_profile_id) REFERENCES ${myuniversity}_${mymodule}.mapping_profiles(id),
