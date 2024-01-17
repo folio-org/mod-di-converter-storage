b411d3225a7a412473a595845df0a3872cdb4657
Pavlo Smahin <Pavlo_Smahin@epam.com>
2024-01-17 11:13:34 +0200

    MODDICONV-364: Adjust default QM Authority Create profile to new format (#39)

--- mod-di-converter-storage-server/src/main/resources/templates/db_scripts/defaultData/default_qm_authority_create_job_profile.sql
+++ mod-di-converter-storage-server/src/main/resources/templates/db_scripts/defaultData/default_qm_authority_create_job_profile.sql
@@ -30,7 +30,7 @@ INSERT INTO ${myuniversity}_${mymodule}.action_profiles (id, jsonb) values
      "action": "CREATE",
      "deleted": false,
      "hidden": true,
-       "remove9Subfields": false,
+     "remove9Subfields": false,
      "metadata": {
        "createdDate": "2020-11-30T09:02:39.96",
        "updatedDate": "2020-11-30T11:57:24.083+0000",
@@ -214,13 +214,22 @@ INSERT INTO ${myuniversity}_${mymodule}.mapping_profiles (id, jsonb) values
 }')
   ON CONFLICT DO NOTHING;
 
+INSERT INTO ${myuniversity}_${mymodule}.profile_wrappers (id, profile_type, job_profile_id) values
+  ('0a416462-f13a-40bd-b6dd-09fdb92971e4', 'JOB_PROFILE', '6eefa4c6-bbf7-4845-ad82-de7fc4abd0e3') ON CONFLICT DO NOTHING;
+
+INSERT INTO ${myuniversity}_${mymodule}.profile_wrappers (id, profile_type, action_profile_id) values
+  ('fea9b307-7121-488e-a09b-e1c9fa670601', 'ACTION_PROFILE', '7915c72e-c7af-4962-969d-403c7238b051') ON CONFLICT DO NOTHING;
+
+INSERT INTO ${myuniversity}_${mymodule}.profile_wrappers (id, profile_type, mapping_profile_id) values
+  ('1fc9af82-6f1e-4ff2-ab15-97c50c3a8d49', 'MAPPING_PROFILE', '6a0ec1de-68eb-4833-bdbf-0741db85c314') ON CONFLICT DO NOTHING;
+
 INSERT INTO ${myuniversity}_${mymodule}.job_to_action_profiles (id, jsonb) values
   ('06915a99-9b58-493d-828b-4ff104ba7e49', '{
   "id": "06915a99-9b58-493d-828b-4ff104ba7e49",
   "order": 0,
   "triggered": false,
-  "detailProfileId": "7915c72e-c7af-4962-969d-403c7238b051",
-  "masterProfileId": "6eefa4c6-bbf7-4845-ad82-de7fc4abd0e3",
+  "detailWrapperId": "fea9b307-7121-488e-a09b-e1c9fa670601",
+  "masterWrapperId": "0a416462-f13a-40bd-b6dd-09fdb92971e4",
   "detailProfileType": "ACTION_PROFILE",
   "masterProfileType": "JOB_PROFILE"
 }')
@@ -232,8 +241,8 @@ INSERT INTO ${myuniversity}_${mymodule}.action_to_mapping_profiles (id, jsonb) v
      "id": "cfde7532-28e0-4bcc-9d3c-54f8853bcba1",
      "order": 0,
      "triggered": false,
-     "detailProfileId": "6a0ec1de-68eb-4833-bdbf-0741db85c314",
-     "masterProfileId": "7915c72e-c7af-4962-969d-403c7238b051",
+     "detailWrapperId": "1fc9af82-6f1e-4ff2-ab15-97c50c3a8d49",
+     "masterWrapperId": "fea9b307-7121-488e-a09b-e1c9fa670601",
      "detailProfileType": "MAPPING_PROFILE",
      "masterProfileType": "ACTION_PROFILE"
    }') ON CONFLICT DO NOTHING;
