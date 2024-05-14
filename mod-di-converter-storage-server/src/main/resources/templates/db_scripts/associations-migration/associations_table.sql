4c79ef54fd3a6ca33700c12a324ff6c939d85d89
JavokhirAbdullayev <Javokhir_Abdullaev@epam.com>
2024-05-14 10:27:46 +0500

    rename order column

--- mod-di-converter-storage-server/src/main/resources/templates/db_scripts/associations-migration/associations_table.sql
+++ mod-di-converter-storage-server/src/main/resources/templates/db_scripts/associations-migration/associations_table.sql
@@ -13,7 +13,6 @@ ALTER TABLE ${myuniversity}_${mymodule}.associations
   ADD IF NOT EXISTS detail_profile_id uuid NULL,
   ADD IF NOT EXISTS master_profile_type text NULL,
   ADD IF NOT EXISTS detail_profile_type text NULL,
-  ADD IF NOT EXISTS order integer NULL,
-
+  ADD IF NOT EXISTS detail_order integer NULL,
   ADD CONSTRAINT associations_master_wrapper_fk FOREIGN KEY (master_wrapper_id) REFERENCES ${myuniversity}_${mymodule}.profile_wrappers(id),
-  ADD CONSTRAINT associations_detail_wrapper_fk FOREIGN KEY (detail_wrapper_id) REFERENCES ${myuniversity}_${mymodule}.profile_wrappers(id),
+  ADD CONSTRAINT associations_detail_wrapper_fk FOREIGN KEY (detail_wrapper_id) REFERENCES ${myuniversity}_${mymodule}.profile_wrappers(id);
