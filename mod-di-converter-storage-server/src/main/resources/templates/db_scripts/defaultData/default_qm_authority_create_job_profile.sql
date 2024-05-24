b345ad42811087853657e630e74fbc7711d8ac63
JavokhirAbdullayev <Javokhir_Abdullaev@epam.com>
2024-05-24 18:38:52 +0500

    fix tests

--- mod-di-converter-storage-server/src/main/resources/templates/db_scripts/defaultData/default_qm_authority_create_job_profile.sql
+++ mod-di-converter-storage-server/src/main/resources/templates/db_scripts/defaultData/default_qm_authority_create_job_profile.sql
@@ -214,6 +214,15 @@ INSERT INTO ${myuniversity}_${mymodule}.mapping_profiles (id, jsonb) values
 }')
   ON CONFLICT DO NOTHING;
 
+INSERT INTO ${myuniversity}_${mymodule}.profile_wrappers (id, profile_type, job_profile_id) values
+  ('0a416462-f13a-40bd-b6dd-09fdb92971e4', 'JOB_PROFILE', '6eefa4c6-bbf7-4845-ad82-de7fc4abd0e3') ON CONFLICT DO NOTHING;
+
+INSERT INTO ${myuniversity}_${mymodule}.profile_wrappers (id, profile_type, action_profile_id) values
+  ('fea9b307-7121-488e-a09b-e1c9fa670601', 'ACTION_PROFILE', '7915c72e-c7af-4962-969d-403c7238b051') ON CONFLICT DO NOTHING;
+
+INSERT INTO ${myuniversity}_${mymodule}.profile_wrappers (id, profile_type, mapping_profile_id) values
+  ('1fc9af82-6f1e-4ff2-ab15-97c50c3a8d49', 'MAPPING_PROFILE', '6a0ec1de-68eb-4833-bdbf-0741db85c314') ON CONFLICT DO NOTHING;
+
 INSERT INTO ${myuniversity}_${mymodule}.job_to_action_profiles (id, jsonb) values
   ('06915a99-9b58-493d-828b-4ff104ba7e49', '{
   "id": "06915a99-9b58-493d-828b-4ff104ba7e49",
@@ -237,3 +246,33 @@ INSERT INTO ${myuniversity}_${mymodule}.action_to_mapping_profiles (id, jsonb) v
      "detailProfileType": "MAPPING_PROFILE",
      "masterProfileType": "ACTION_PROFILE"
    }') ON CONFLICT DO NOTHING;
+
+INSERT INTO ${myuniversity}_${mymodule}.associations (id, job_profile_id, master_wrapper_id,
+    detail_wrapper_id, master_profile_id, detail_profile_id,
+    master_profile_type, detail_profile_type, detail_order, react_to) values
+  ('06915a99-9b58-493d-828b-4ff104ba7e49',
+   null,
+   '0a416462-f13a-40bd-b6dd-09fdb92971e4',
+   'fea9b307-7121-488e-a09b-e1c9fa670601',
+    null,
+    null,
+   'JOB_PROFILE',
+   'ACTION_PROFILE',
+    0,
+    null
+   ) ON CONFLICT DO NOTHING;
+
+INSERT INTO ${myuniversity}_${mymodule}.associations (id, job_profile_id, master_wrapper_id,
+    detail_wrapper_id, master_profile_id, detail_profile_id,
+    master_profile_type, detail_profile_type, detail_order, react_to) values
+  ('cfde7532-28e0-4bcc-9d3c-54f8853bcba1',
+   null,
+   'fea9b307-7121-488e-a09b-e1c9fa670601',
+   '1fc9af82-6f1e-4ff2-ab15-97c50c3a8d49',
+   null,
+   null,
+   'ACTION_PROFILE',
+   'MAPPING_PROFILE',
+   0,
+   null
+  ) ON CONFLICT DO NOTHING;
