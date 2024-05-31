fe3363b983029484e9dd6f09812395f2faedc9aa
JavokhirAbdullayev <Javokhir_Abdullaev@epam.com>
2024-05-31 12:25:09 +0500

    move associations related migrations into separate folders

--- mod-di-converter-storage-server/src/main/resources/templates/db_scripts/associations_view.sql
+++ mod-di-converter-storage-server/src/main/resources/templates/db_scripts/associations_view.sql
@@ -4,45 +4,53 @@
 CREATE OR REPLACE VIEW associations_view
   AS
     SELECT association_id, master_id, masterwrapperid, master, master_type, detail_id, detailwrapperid, detail_type, detail_order, detail, react_to, job_profile_id
-    FROM get_profile_association_snapshot('job_profiles', 'JOB_PROFILE', 'match_profiles', 'MATCH_PROFILE')
+    FROM get_profile_association_snapshot('job_to_match_profiles', 'job_profiles', 'JOB_PROFILE', 'match_profiles', 'MATCH_PROFILE')
       UNION ALL
     SELECT association_id, master_id, masterwrapperid, master, master_type, detail_id, detailwrapperid, detail_type, detail_order, detail, react_to, job_profile_id
-    FROM get_profile_association_snapshot('job_profiles', 'JOB_PROFILE', 'action_profiles', 'ACTION_PROFILE')
+    FROM get_profile_association_snapshot('job_to_action_profiles', 'job_profiles', 'JOB_PROFILE', 'action_profiles', 'ACTION_PROFILE')
       UNION ALL
     SELECT association_id, master_id, masterwrapperid, master, master_type, detail_id, detailwrapperid, detail_type, detail_order, detail, react_to, job_profile_id
-    FROM get_profile_association_snapshot('match_profiles', 'MATCH_PROFILE', 'match_profiles', 'MATCH_PROFILE')
+    FROM get_profile_association_snapshot('match_to_match_profiles', 'match_profiles', 'MATCH_PROFILE', 'match_profiles', 'MATCH_PROFILE')
      UNION ALL
     SELECT association_id, master_id, masterwrapperid, master, master_type, detail_id, detailwrapperid, detail_type, detail_order, detail, react_to, job_profile_id
-    FROM get_profile_association_snapshot('match_profiles','MATCH_PROFILE', 'action_profiles', 'ACTION_PROFILE')
+    FROM get_profile_association_snapshot('match_to_action_profiles', 'match_profiles','MATCH_PROFILE', 'action_profiles', 'ACTION_PROFILE')
      UNION ALL
     SELECT association_id, master_id, masterwrapperid, master, master_type, detail_id, detailwrapperid, detail_type, detail_order, detail, react_to, job_profile_id
-    FROM get_profile_association_snapshot('action_profiles', 'ACTION_PROFILE', 'match_profiles', 'MATCH_PROFILE')
+    FROM get_profile_association_snapshot('action_to_match_profiles', 'action_profiles', 'ACTION_PROFILE', 'match_profiles', 'MATCH_PROFILE')
       UNION ALL
     SELECT association_id, master_id, masterwrapperid, master, master_type, detail_id, detailwrapperid, detail_type, detail_order, detail, react_to, job_profile_id
-    FROM get_profile_association_snapshot('action_profiles', 'ACTION_PROFILE', 'action_profiles', 'ACTION_PROFILE')
+    FROM get_profile_association_snapshot('action_to_action_profiles', 'action_profiles', 'ACTION_PROFILE', 'action_profiles', 'ACTION_PROFILE')
       UNION ALL
     SELECT association_id, master_id, masterwrapperid, master, master_type, detail_id, detailwrapperid, detail_type, detail_order, detail, react_to, job_profile_id
-    FROM get_profile_association_snapshot('action_profiles', 'ACTION_PROFILE', 'mapping_profiles', 'MAPPING_PROFILE')
+    FROM get_profile_association_snapshot('action_to_mapping_profiles', 'action_profiles', 'ACTION_PROFILE', 'mapping_profiles', 'MAPPING_PROFILE')
       UNION ALL
-    SELECT profile_associations.id AS association_id, profile_associations.job_profile_id AS master_id, profile_associations.master_wrapper_id AS masterwrapperid, json_agg(job.jsonb) AS master, 'JOB_PROFILE' AS master_type, detail.id AS detail_id, profile_associations.detail_wrapper_id AS detailwrapperid, 'ACTION_PROFILE' AS detail_type, 0 AS detail_order, json_agg(detail.jsonb) AS detail, null AS react_to, profile_associations.job_profile_id AS job_profile_id
-    FROM profile_associations
-    INNER JOIN profile_wrappers masterwrapper ON profile_associations.master_wrapper_id = masterwrapper.id
-    INNER JOIN profile_wrappers detailwrapper ON profile_associations.detail_wrapper_id = detailwrapper.id
-    INNER JOIN match_profiles master ON master.id = masterwrapper.match_profile_id
-    INNER JOIN action_profiles detail ON detail.id = detailwrapper.action_profile_id
-    INNER JOIN job_profiles job ON job.id = profile_associations.job_profile_id
-    WHERE profile_associations.detail_profile_type = 'ACTION_PROFILE'
-    	AND profile_associations.master_profile_type = 'MATCH_PROFILE'
-    GROUP BY profile_associations.id, master.id, detail.id, detail.jsonb;
+    SELECT ASSOCIATION.ID AS association_id, CAST(ASSOCIATION.JSONB ->> 'jobProfileId' AS UUID) AS master_id, ASSOCIATION.MASTERWRAPPERID, json_agg(JOB.jsonb) AS master, 'JOB_PROFILE' AS master_type, DETAIL.ID AS detail_id, ASSOCIATION.DETAILWRAPPERID, 'ACTION_PROFILE' AS detail_type, 0 AS detail_order, json_agg(DETAIL.JSONB) AS detail, null AS react_to, CAST(ASSOCIATION.JSONB ->> 'jobProfileId' AS UUID) AS job_profile_id
+    FROM MATCH_TO_ACTION_PROFILES ASSOCIATION
+    INNER JOIN PROFILE_WRAPPERS MASTERWRAPPER ON ASSOCIATION.MASTERWRAPPERID = MASTERWRAPPER.ID
+    INNER JOIN PROFILE_WRAPPERS DETAILWRAPPER ON ASSOCIATION.DETAILWRAPPERID = DETAILWRAPPER.ID
+    INNER JOIN MATCH_PROFILES MASTER ON MASTER.ID = MASTERWRAPPER.MATCH_PROFILE_ID
+    INNER JOIN ACTION_PROFILES DETAIL ON DETAIL.ID = DETAILWRAPPER.ACTION_PROFILE_ID
+    INNER JOIN JOB_PROFILES JOB ON JOB.ID = CAST(ASSOCIATION.JSONB ->> 'jobProfileId' AS UUID)
+    WHERE ASSOCIATION.JSONB ->> 'detailProfileType' = 'ACTION_PROFILE'
+    	AND ASSOCIATION.JSONB ->> 'masterProfileType' = 'MATCH_PROFILE'
+    GROUP BY ASSOCIATION.ID, MASTER.ID, DETAIL.ID, DETAIL.JSONB;
 
 -- Script to create rule which will triggered upon delete query to associations_view view.
 CREATE OR REPLACE RULE delete_associations_with_details AS
   ON DELETE TO associations_view
   DO INSTEAD (
-    DELETE FROM profile_associations WHERE OLD.master_id IN
-      (SELECT action_profile_id FROM profile_wrappers WHERE id = profile_associations.master_wrapper_id);
-    DELETE FROM profile_associations WHERE OLD.master_id IN
-      (SELECT job_profile_id FROM profile_wrappers WHERE id = profile_associations.master_wrapper_id);
-    DELETE FROM profile_associations WHERE OLD.master_id IN
-      (SELECT match_profile_id FROM profile_wrappers WHERE id = profile_associations.master_wrapper_id);
+    DELETE FROM action_to_action_profiles WHERE OLD.master_id IN
+      (SELECT action_profile_id FROM profile_wrappers WHERE id = action_to_action_profiles.masterwrapperid);
+    DELETE FROM action_to_mapping_profiles WHERE OLD.master_id IN
+      (SELECT action_profile_id FROM profile_wrappers WHERE id = action_to_mapping_profiles.masterwrapperid);
+    DELETE FROM action_to_match_profiles WHERE OLD.master_id IN
+      (SELECT action_profile_id FROM profile_wrappers WHERE id = action_to_match_profiles.masterwrapperid);
+    DELETE FROM job_to_action_profiles WHERE OLD.master_id IN
+      (SELECT job_profile_id FROM profile_wrappers WHERE id = job_to_action_profiles.masterwrapperid);
+    DELETE FROM job_to_match_profiles WHERE OLD.master_id IN
+      (SELECT job_profile_id FROM profile_wrappers WHERE id = job_to_match_profiles.masterwrapperid);
+    DELETE FROM match_to_action_profiles WHERE OLD.master_id IN
+      (SELECT match_profile_id FROM profile_wrappers WHERE id = match_to_action_profiles.masterwrapperid);
+    DELETE FROM match_to_match_profiles WHERE OLD.master_id IN
+      (SELECT match_profile_id FROM profile_wrappers WHERE id = match_to_match_profiles.masterwrapperid);
     );
