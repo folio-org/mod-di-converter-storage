80d642f9ac0c244750cef32bdcde200001769273
Javokhir Abdullaev <javokhir_abdullaev@epam.com>
2024-06-11 19:27:06 +0500

    revoke reorder changes

--- mod-di-converter-storage-server/src/main/java/org/folio/rest/impl/ModTenantAPI.java
+++ mod-di-converter-storage-server/src/main/java/org/folio/rest/impl/ModTenantAPI.java
@@ -75,7 +75,6 @@ public class ModTenantAPI extends TenantAPI {
   Future<Integer> loadData(TenantAttributes attributes, String tenantId, Map<String, String> headers, Context context) {
     return super.loadData(attributes, tenantId, headers, context)
       .compose(num -> runSqlScript(DEFAULT_JOB_PROFILE_SQL, headers, context)
-        .compose(m -> profileMigrationService.migrateDataImportProfiles(headers, context))
         .compose(r -> runSqlScript(DEFAULT_MARC_FIELD_PROTECTION_SETTINGS_SQL, headers, context))
         .compose(d -> runSqlScript(DEFAULT_OCLC_JOB_PROFILE_SQL, headers, context))
         .compose(u -> runSqlScript(DEFAULT_OCLC_UPDATE_JOB_PROFILE_SQL, headers, context))
@@ -97,6 +96,7 @@ public class ModTenantAPI extends TenantAPI {
         .compose(m -> runSqlScript(DEFAULT_QM_AUTHORITY_UPDATE_JOB_PROFILE, headers, context))
         .compose(m -> runSqlScript(DEFAULT_QM_MARC_BIB_UPDATE_JOB_PROFILE, headers, context))
         .compose(m -> runSqlScript(DEFAULT_QM_HOLDINGS_UPDATE_JOB_PROFILE, headers, context))
+        .compose(m -> profileMigrationService.migrateDataImportProfiles(headers, context))
         .compose(m -> runSqlScript(DEFAULT_ECS_INSTANCE_AND_MARC_BIB_CREATE_JOB_PROFILE, headers, context))
         .compose(m -> runSqlScript(DEFAULT_QM_AUTHORITY_CREATE_JOB_PROFILE, headers, context))
         .map(num));
