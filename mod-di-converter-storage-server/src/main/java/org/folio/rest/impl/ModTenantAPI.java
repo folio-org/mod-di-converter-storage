0ddf0f621155510946674080b09fe55cbfd0a0ff
Javokhir Abdullaev <101543142+JavokhirAbdullayev@users.noreply.github.com>
2024-06-26 17:27:42 +0500

    fix associations between default profiles (#61)

--- mod-di-converter-storage-server/src/main/java/org/folio/rest/impl/ModTenantAPI.java
+++ mod-di-converter-storage-server/src/main/java/org/folio/rest/impl/ModTenantAPI.java
@@ -74,7 +74,8 @@ public class ModTenantAPI extends TenantAPI {
   @Override
   Future<Integer> loadData(TenantAttributes attributes, String tenantId, Map<String, String> headers, Context context) {
     return super.loadData(attributes, tenantId, headers, context)
-      .compose(num -> runSqlScript(DEFAULT_JOB_PROFILE_SQL, headers, context)
+      .compose(num -> profileMigrationService.migrateDataImportProfiles(headers, context)
+        .compose(m -> runSqlScript(DEFAULT_JOB_PROFILE_SQL, headers, context))
         .compose(r -> runSqlScript(DEFAULT_MARC_FIELD_PROTECTION_SETTINGS_SQL, headers, context))
         .compose(d -> runSqlScript(DEFAULT_OCLC_JOB_PROFILE_SQL, headers, context))
         .compose(u -> runSqlScript(DEFAULT_OCLC_UPDATE_JOB_PROFILE_SQL, headers, context))
@@ -98,7 +99,6 @@ public class ModTenantAPI extends TenantAPI {
         .compose(m -> runSqlScript(DEFAULT_QM_HOLDINGS_UPDATE_JOB_PROFILE, headers, context))
         .compose(m -> runSqlScript(DEFAULT_ECS_INSTANCE_AND_MARC_BIB_CREATE_JOB_PROFILE, headers, context))
         .compose(m -> runSqlScript(DEFAULT_QM_AUTHORITY_CREATE_JOB_PROFILE, headers, context))
-        .compose(m -> profileMigrationService.migrateDataImportProfiles(headers, context))
         .map(num));
   }
 
