ddf95a5e2748a552905b6dcdaca84f3e8e7ce76f
Roman_Chernetskyi <Roman_Chernetskyi@epam.com>
2025-02-19 16:42:24 +0200

    Change mapping field index

--- mod-di-converter-storage-server/src/main/resources/templates/db_scripts/data-migration/add_set_for_deletion_mapping.sql
+++ mod-di-converter-storage-server/src/main/resources/templates/db_scripts/data-migration/add_set_for_deletion_mapping.sql
@@ -1,3 +1,3 @@
 UPDATE ${myuniversity}_${mymodule}.mapping_profiles
-SET jsonb = jsonb_insert(jsonb, '{mappingDetails, mappingFields, 44}', '{"name": "deleted", "enabled": "true", "path": "instance.deleted", "booleanFieldAction": "ALL_FALSE", "subfields": []}')
+SET jsonb = jsonb_insert(jsonb, '{mappingDetails, mappingFields, 0}', '{"name": "deleted", "enabled": "true", "path": "instance.deleted", "booleanFieldAction": "ALL_FALSE", "subfields": []}')
 WHERE jsonb -> 'mappingDetails' ->> 'recordType' = 'INSTANCE';
